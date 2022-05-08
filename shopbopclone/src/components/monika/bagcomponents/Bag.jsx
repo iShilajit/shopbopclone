75b40cc1d6092e90f94bcb4b7bac9579e2fc394d
monikasananse <monikasnns@gmail.com>
2022-05-09 01:18:00 +0530

    Done By Monika
    Accessories page and Bag Page

---
+++ shopbopclone/src/components/monika/bagcomponents/Bag.jsx
@@ -0,0 +1,54 @@
+// import React, { useState ,useEffect} from 'react'
+import {Good,Divi,Main}from "./Bag.styled.js";
+import { BagData } from './BagData.js';
+
+const Bag = () => {
+  //console.log(BagData);
+  // const[data,setData]=useState([]);
+
+  // useEffect(()=>{
+  //   const getData=async()=>{
+  //     try{
+  //       let response=await fetch("http://localhost:2030/bags/");
+  //       let data=await response.json();
+  //       console.log(data);
+  //       setData(data);
+  //     }catch(err){
+  //       console.log("err:",err)
+  //     }
+  //   }
+  //   getData();
+  // },[])
+  
+  return (
+    <>
+    <div>
+      <img style={{height:"50px"}}src="https://m.media-amazon.com/images/G/01/Shopbop/p/pcs/shopbop/media/3/images/logos/rebrand_shopbop_logo_2x_1-3.png"/>
+    </div>
+    <h1 style={{fontStyle:"Helvetica-Neue,sans-serif",fontSize:"20px"}}>POPULAR PICKS FOR YOU</h1>
+    
+      
+        <Divi>
+            <Good>
+
+            {BagData.map((i)=>{
+              return (
+                <div key={i.id}>
+                    <img src={i.imgURL} alt="img"/>
+                    <p style={{fontStyle:"sans-serif",fontWeight:"bold",fontSize:"15px"}}>{i.title}</p>
+                    <p style={{fontStyle:"sans-serif",fontSize:"16px"}}>{i.name}</p>
+                    <p style={{fontWeight:"bold"}}>{i.price}</p>
+
+                </div>
+              )
+            })}
+            
+            </Good>
+        </Divi>
+       
+      
+    </>
+  )
+}
+
+export default Bag;
