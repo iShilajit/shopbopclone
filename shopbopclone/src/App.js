94a91b3c5e6709db13b7b0b067a9296366f6d604
Shilajit <shilajit2014paul@gmail.com>
2022-05-09 18:43:34 +0530

    made some minor changes

--- shopbopclone/src/App.js
+++ shopbopclone/src/App.js
@@ -1,18 +1,6 @@
 
 import './App.css';
-// import { Checkout } from './components/shilajit/Checkout';
-// import MultipleItemsTwo from './components/shilajit/Carousal/Slider';
-// import { Fav } from './components/shilajit/Fav';
-// import  Home  from './components/shilajit/Home';
-// import { Login } from './components/shilajit/Login';
-// import Example from './components/shilajit/Navbar';
-// import { Product } from './components/shilajit/Product';
-// import { Signup } from './components/shilajit/Signup';
 
-// import { Description } from './components/shilajit/Description';
-// import Multithings from './components/monika/accessories/Multithings';
-// import Bag from './components/monika/bagcomponents/Bag';
-// import { Mens } from './components/sridhar/Mens';
 import MainRoutes from './components/shilajit/Routing/Mainroutes';
 
 
@@ -24,19 +12,7 @@ function App() {
 
   <MainRoutes/>
 
-     {/* <Example/> */}
-     {/* <Signup/> */}
-     {/* <Login/> */}
-    {/* <Home/> */}
-    {/* <MultipleItemsTwo/> */}
-    {/* <Product/> */}
-    {/* <Fav/> */}
-    
-{/* <Multithings/> */}
-{/* <Bag/> */}
-{/* <Mens/> */}
-{/* <Description/> */}
-{/* <Checkout/> */}
+
 
 
     </div>
